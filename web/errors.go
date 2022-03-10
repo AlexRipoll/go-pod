e6b5749341f9f44dbba7842e007c6dac1e81dd18
Alex Ripoll <alexripollpiera@gmail.com>
2022-03-10 19:04:38 +0100

    adding web response functionality

--- web/errors.go
+++ web/errors.go
@@ -5,30 +5,31 @@ import (
 	"net/http"
 )
 
-// ErrorResponse is the form used for API responses from failures in the API.
-type ErrorResponse struct {
+// Error is the form used for API responses from failures in the API.
+type Error struct {
 	Message string `json:"message"`
 	Status  int    `json:"status"`
 }
 
 // ErrorMatch checks the error's flag and creates a REST error response to return for the API request.
-func ErrorMatch(err error) ErrorResponse {
-	var er ErrorResponse
+func ErrorMatch(err error) Error {
+	var er Error
 	flaggedErr := err.(errorFlag.Flagged)
 
 	switch flaggedErr.Flag() {
 	case errorFlag.InvalidData, errorFlag.AlreadyExists:
-		er = ErrorResponse{
+		er = Error{
 			Message: flaggedErr.Unwrap().Error(),
 			Status:  http.StatusBadRequest,
 		}
 	case errorFlag.NotFound:
-		er = ErrorResponse{
+		er = Error{
 			Message: flaggedErr.Unwrap().Error(),
 			Status:  http.StatusNotFound,
 		}
 	case errorFlag.Internal:
-		er = ErrorResponse{
+		er = Error{
+			//Message: "internal server error",
 			Message: flaggedErr.Unwrap().Error(),
 			Status:  http.StatusInternalServerError,
 		}
---
+++ web/response.go
@@ -0,0 +1,55 @@
+package web
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+// Response parses the data to JSON and sets the HTTP Headers and sends it to the client.
+func Response(ctx context.Context, w http.ResponseWriter, data interface{}, statusCode int) error {
+
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		//return errorFlag.New(err, errorFlag.Internal)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(statusCode)
+
+	if _, err := w.Write(jsonData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ErrorResponse checks if the received error has errorFlag.flagged type, parses to JSON and
+// returns it to the client.
+func ErrorResponse(ctx context.Context, w http.ResponseWriter, error error) {
+
+	errorData := ErrorMatch(error)
+
+	jsonData, err := json.Marshal(errorData)
+	if err != nil {
+		//return errorFlag.New(err, errorFlag.Internal)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(errorData.Status)
+
+	if _, err := w.Write(jsonData); err != nil {
+		return
+	}
+
+	return
+}
